aaacfb5a52def627a3a5a58165e7ff6e903ffd2f
ChiragAjmera57 <chiragajmera57@gmail.com>
2023-05-17 14:39:40 +0530

    initial commi

--- controllers/products.js
+++ controllers/products.js
@@ -1,30 +1,26 @@
-const Product = require("../models/products")
-const getallproducts = async (req,res)=>{
-    const {name,mrp,brand,usesof,sort} = req.query;
-    const queryobject = {}
-    if(name)
-    {
-        queryobject.name = { $regex: name, $option: "i"};
-    }
-    if(mrp)
-    {
-        queryobject.mrp = mrp;
-    }
-    if(brand)
-    {
-        queryobject.brand = brand;
-    }
-    let apidata = Product.find(queryobject)
-    if(sort)
-    {
-        let sortfix = sort.replace(","," ")
-        apidata = apidata.sort(sortfix)
-    }
-    const mydata = await apidata
-    res.status(200).json({mydata});
-}
-const getalltesting = async (req,res)=>{
-    const mydata = await Product.find(req.query)
-    res.status(200).json({mydata});
-}
-module.exports = {getallproducts,getalltesting};
+const Product = require("../models/products");
+const getallproducts = async (req, res) => {
+  const { name, mrp, brand, usesof, sort } = req.query;
+  const queryobject = {};
+  if (name) {
+    queryobject.name = { $regex: name, $options: 'i' };
+  }
+  if (mrp) {
+    queryobject.mrp = mrp;
+  }
+  if (brand) {
+    queryobject.brand = brand;
+  }
+  let apidata = Product.find(queryobject);
+  if (sort) {
+    let sortfix = sort.replace(",", " ");
+    apidata = apidata.sort(sortfix);
+  }
+  const mydata = await apidata;
+  res.status(200).json({ mydata });
+};
+const getalltesting = async (req, res) => {
+  const mydata = await Product.find(req.query);
+  res.status(200).json({ mydata });
+};
+module.exports = { getallproducts, getalltesting };
